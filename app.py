96fe44aed5e6be3e8fab5b828def999e69b69b4e
shuflovic <pavel.skuta@gmail.com>
2025-09-19 22:52:21 +1200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("testing")
 
 data = pd.read_csv("data.csv")
 
-filtered_data = data[(data['country'] == 'sweden') | (data['country'] == 'slovakia') ]
+filtered_data = data[(data['country'] == 'sweden') | (data['platform'] == 'workaway') ]
 
 st.write("two countries only")
 st.write(filtered_data)
