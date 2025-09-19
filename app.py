c6d65feb41051e4d6c5436ddf4d68cc8f482bac0
shuflovic <pavel.skuta@gmail.com>
2025-09-19 22:43:57 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-st.title("CSV File Uploader")
-
+st.title("testing")
 
 data = pd.read_csv("data.csv")
+
+sweden_data = data[data['country'] == 'Sweden']
+
+st.write("sweden only")
+st.write(sweden_data)
+
+st.write("all data")
 st.write(data)
 
 
