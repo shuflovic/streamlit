2b62ee4ae1e10bbdbdb2a30cd74e2baec4df412e
shuflovic <pavel.skuta@gmail.com>
2025-09-19 22:59:18 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("testing")
 
@@ -7,11 +8,21 @@ data = pd.read_csv("data.csv")
 
 filtered_data = data[(data['country'] == 'sweden') & (data['platform'] == 'workaway') ]
 
-st.write("two countries only")
-st.write(filtered_data)
 
-st.write("all data")
-st.write(data)
+location_nights = data.groupby('location')['nights'].sum()
+
+fig, ax = plt.subplots()
+ax.pie(location_nights, labels=location_nights.index, autopct='%1.1f%%', startangle=90)
+ax.axis('equal')  # Ensures the pie chart is a circle
+
+st.write("Nights Spent per Location:")
+st.pyplot(fig)
+
+#st.write("two countries only")
+#st.write(filtered_data)
+
+#st.write("all data")
+#st.write(data)
 
 
     
