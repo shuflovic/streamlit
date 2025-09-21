c6fb47ddb0152b20e0701dcb8a9110a056b7d2b7
shuflovic <pavel.skuta@gmail.com>
2025-09-21 22:41:00 +1200

    Update app.py

--- app.py
+++ app.py
@@ -164,12 +164,12 @@ with third_col2:
 forth_col1, forth_col2 = st.tabs(["list", "map"])
 st.title("visited countries")
 with forth_col1:
-    data = data.groupby(['country'])['nights']
-            .sum()
-            .reset_index()
-        )
-    result.index = range(1, len(result) + 1)
-    st.dataframe(data, use_container_width=True, hide_index=False)
+  data = data.groupby(['country'])['nights']
+  .sum()
+  .reset_index()
+)
+result.index = range(1, len(result) + 1)
+st.dataframe(data, use_container_width=True, hide_index=False)
 
 with forth_col2:
     st.write("map")
