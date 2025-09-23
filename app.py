7257a1094c461936be60c0c5d7adf9b335bd72b0
shuflovic <pavel.skuta@gmail.com>
2025-09-24 11:32:52 +1200

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ with forth_col2:
     activity_days = {
         'treeplanting': data[data['platform'] == 'treeplanting']['nights'].sum(),
         'Workaway': data[data['platform'] == 'workaway']['nights'].sum(),
-        'kungsleden': data[data['location'] == 'kungsleden']['nights'].sum(),
+        'kungsleden': data[data['platform'] == 'kungsleden']['nights'].sum(),
         'paid accommodation': data[data['platform'].str.contains('booking|stf|airbnb|trip.com', case=False, na=False, regex=True)]['nights'].sum(),
         'other': data[data['platform'].str.contains('airport|plane|train|flight|transfer', case=False, na=False, regex=True)]['nights'].sum()
     }
